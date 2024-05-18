fe7e47bb677efd4f37aff9764010cc9da4d4684a
Alyaqdhan <alyaqdhan690s@gmail.com>
2024-05-18 12:11:51 +0400

    removed alert component bottom margin

--- client/src/components/Alert.jsx
+++ client/src/components/Alert.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Alert({text, color='alert-danger'}) {
   return (
-    <ul className={'py-2 px-5 alert ' + color}>
+    <ul className={'py-2 px-5 mb-0 alert ' + color}>
       <li><h5>{text}</h5></li>
     </ul>
   )
